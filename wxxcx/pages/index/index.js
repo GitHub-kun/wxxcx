f9930926009df7b543ed5036d430a634253950a5
wanglikun <523426851@>
2019-02-19 10:59:58 +0800

    更新

--- wxxcx/pages/index/index.js
+++ wxxcx/pages/index/index.js
@@ -14,13 +14,22 @@ Page({
     staffB: { firstName: 'Shang', lastName: 'You' },
     staffC: { firstName: 'Gideon', lastName: 'Lin' },
     count:1,
-    latitude :'', //纬度
-    longitude:'' //经度
+    latitude:'34.808615', //纬度
+    longitude:'113.551238', //经度
+    speed: 0,
+    accuracy: 0
   },
   add(e){
     this.setData({
       count:this.data.count + 1
     })
+    wx.scanCode({
+      onlyFromCamera: false,
+      scanType: ['qrCode'],
+      success: (res) => {
+        console.log(res)
+      }
+    });
   },
   //事件处理函数
   bindViewTap: function() {
@@ -29,6 +38,41 @@ Page({
     })
   },
   onLoad: function () {
+   
+    var that = this;
+    wx.showLoading({
+      title: "定位中",
+      mask: true
+    });
+    
+    wx.getLocation({
+      type: 'gcj02',
+      altitude: true,//高精度定位
+      //定位成功，更新定位结果
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        var speed = res.speed
+        var accuracy = res.accuracy
+        that.setData({
+          longitude: longitude,
+          latitude: latitude,
+          speed: speed,
+          accuracy: accuracy
+        })
+      },
+      //定位失败回调
+      fail: function () {
+        wx.showToast({
+          title: "定位失败",
+          icon: "none"
+        })
+      },
+      complete: function () {
+        //隐藏定位中信息进度
+        wx.hideLoading()
+      }
+    });
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
